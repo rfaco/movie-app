83d2a46ae2f19fa7dfb0e7a959769861f8716c04
Roger Faco <rfacodev@gmail.com>
2021-03-15 23:37:26 -0300

    Fixing Id Search

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=ed8077ba440b515d7eeaa90baacf98d4&query="'
 
 /* Search form */
+const main = document.getElementById('main')
 const form = document.getElementById('form')
-const search   = document.getElementById('search')
+const search = document.getElementById('search')
 
 
 /* Get Movies */
@@ -14,9 +15,43 @@ async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
 
-  console.log(data.results);
+  showMovies(data.results);
 }
 
+function showMovies(movies) {
+  main.innerHTML = ''
+
+
+  movies.forEach((movie) => {
+    const {title, poster_path, vote_average, overview } = movie
+    
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+
+    movieEl.innerHTML = `
+     <img src="${IMG_PATH + poster_path}" alt="${title}">
+     <div class="movie-info">
+       <h3>${title}</h3>
+       <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+     </div>
+     <div class="overview">
+       <h3>Overview</h3>
+       ${overview}
+     </div>`
+   
+   main.appendChild(movieEl)
+  })
+}
+
+function getClassByRate(vote) {
+  if(vote >= 8) {
+    return 'green'
+  }else if(vote >= 5) {
+    return 'orange'
+  }else {
+    return'red'
+  }
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
